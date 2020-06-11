c267875d2a4f5df55a089e09b9d6be947df78ef7
Bayu Adi Wibowo <bayu.eighted@gmail.com>
2020-06-11 11:37:24 +0700

    use enzyme for test components

--- app/containers/Home/tests/index.test.js
+++ app/containers/Home/tests/index.test.js
@@ -9,19 +9,29 @@
 import React from "react";
 import { render } from "react-testing-library";
 // import 'jest-dom/extend-expect'; // add some helpful assertions
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
+import { shallow } from "enzyme";
 import { Home } from "../index";
+import { Button } from "antd";
+
+configure({ adapter: new Adapter() });
 
 describe("<Home />", () => {
-  it("Expect to not log errors in console", () => {
+  it.skip("Expect to not log errors in console", () => {
     const spy = jest.spyOn(global.console, "error");
     const dispatch = jest.fn();
     render(<Home dispatch={dispatch} />);
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it("Expect to have additional unit tests specified", () => {
-    expect(true).toEqual(false);
+  it("it should call handleClick", () => {
+    const handleClick = jest.fn();
+    const id = "Iphone 7";
+    const wrapper = shallow(<Button onClick={handleClick(id)} />);
+    wrapper.find("button").simulate("click");
+    expect(handleClick).toBeCalledWith(id);
   });
 
   /**
